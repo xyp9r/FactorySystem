b52aff3a7f7c630ae9d4f9085d91b66e75397f62
xyp9r <vanaradimov@gmail.com>
2026-09-18 21:19:21 +0200

    feat: add JWT token generation for login

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using FactorySystem;
 using Microsoft.EntityFrameworkCore;
 using FactorySystem.Data;
 using FactorySystem.Models;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -111,7 +114,7 @@ app.MapPut("/api/factory/details/{id}", (int id, CreateDetailDto dto, AppDbConte
 });
 
 // Проверка на безопасность вход 
-app.MapPost("/api/factory/login", (AppDbContext db, LoginDto ldto) =>
+app.MapPost("/api/factory/login", (AppDbContext db, LoginDto ldto, IConfiguration config) =>
 {
     var user = db.Users.FirstOrDefault(u => u.Name == ldto.Name);
 
@@ -125,7 +128,21 @@ app.MapPost("/api/factory/login", (AppDbContext db, LoginDto ldto) =>
         return Results.Unauthorized();
     }
         
-    return Results.Ok("Успешный вход!");
+    var key = config["JwtSecret"]!;
+    var secretBytes = System.Text.Encoding.UTF8.GetBytes(key);
+    
+    var securityKey = new SymmetricSecurityKey(secretBytes);
+    var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+    List<Claim> claims = new List<Claim>();
+    claims.Add(new Claim(ClaimTypes.Name, user.Name));
+    claims.Add(new Claim(ClaimTypes.Role, user.Role));
+
+    var token = new JwtSecurityToken(issuer: "MyFactory", audience: "MyFactory", claims, expires: DateTime.UtcNow.AddHours(1), signingCredentials: credentials);
+    
+    var jwtString = new JwtSecurityTokenHandler().WriteToken(token);
+    
+    return Results.Ok(new { token = jwtString });
 });
 
 app.Run();
